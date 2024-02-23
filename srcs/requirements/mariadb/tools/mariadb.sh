4bb7cff14a43835e3ae9be1cc1ddd7305f59b61e
DinoMalin <juliancario@orange.fr>
2024-02-23 16:25:01 +0100

    ~ | Finally managed to connect mariadb without error

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-service mysql start;
+# service mysql start;
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then 
 
-mysql -e "CREATE DATABASE IF NOT EXISTS  \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+	echo "Database already exists"
+else
+mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+mysqld --user=mysql --bootstrap << EOF
+USE mysql;
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+GRANT SELECT ON mysql.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+mysqld --defaults-file=/etc/mysql/mariadb.conf.d/50-server.cnf
+fi
+
+# mysql -e "CREATE DATABASE IF NOT EXISTS  \`${MYSQL_DATABASE}\`;"
+# mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# mysql -e "FLUSH PRIVILEGES;"
+
+# mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+# exec mysqld_safe
