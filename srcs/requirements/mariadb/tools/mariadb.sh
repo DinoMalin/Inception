243704240121106d126c8c3b116321a8c6213e17
DinoMalin <juliancario@orange.fr>
2024-03-06 15:47:06 +0100

    ~ | Clean

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -20,12 +20,3 @@ EOF
 fi
 mysqld --defaults-file=/etc/mysql/mariadb.conf.d/50-server.cnf
 
-
-# mysql -e "CREATE DATABASE IF NOT EXISTS  \`${MYSQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-# mysql -e "FLUSH PRIVILEGES;"
-
-# mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-# exec mysqld_safe
